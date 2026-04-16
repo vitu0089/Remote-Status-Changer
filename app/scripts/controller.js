5f838d85f8876bf20f87e2984e8e4888d53cd69a
vitu0089 <gamerw30@gmail.com>
2026-04-16 16:42:24 +0200

    Reconnect on controller

--- app/scripts/controller.js
+++ app/scripts/controller.js
@@ -1,7 +1,8 @@
 let currentMap = new Map()
 const host = window.location.host
-const socket = new WebSocket(`ws://${document.location.hostname}:61235`)
-
+let socket = new WebSocket(`ws://${document.location.hostname}:61235`)
+const body = document.getElementById("body")
+const originalBodyColor = window.getComputedStyle(body).backgroundColor
 async function GetImageData() {
     let body = await fetch(`http://${host}/controls/imageData`)
     return await body.json()
@@ -27,6 +28,11 @@ async function UpdateList() {
 
         // Clickability
         node.onclick = function(event) {
+            if (socket.readyState == 3 /* 3 means closed */) {
+                console.error("Socket has been closed, please wait for server to restart")
+                return
+            }
+            
             // Force server to change image
             socket.send(item.Name)
         }
@@ -37,24 +43,35 @@ async function UpdateList() {
 UpdateList()
 
 // Socket connections
-socket.addEventListener("open", () => {
-    console.log("Socket connected successfully")
-})
-socket.addEventListener("message", (message) => {
-    let selectedImage = message.data
-    let imageAvailable = selectedImage != "None"
-    let displayNode = document.getElementById("displayImage")
-    if (!displayNode) {
-        console.warn("Unable to find displaynode")
-        return
+function SetupSocketConnection(reboot) {
+    if (reboot) {
+        socket = new WebSocket(`ws://${document.location.hostname}:61235`)
     }
 
-    // Set image
-    const imageJson = JSON.parse(selectedImage)
-    if (imageAvailable) {
-        displayNode.setAttribute("src", `/img/${imageJson.FileName}`)
-    }
+    socket.addEventListener("open", () => {
+        console.log("Socket connected successfully")
+        body.style.backgroundColor = originalBodyColor
+    })
+    socket.addEventListener("message", (message) => {
+        let selectedImage = message.data
+        let imageAvailable = selectedImage != "None"
+        let displayNode = document.getElementById("displayImage")
+        if (!displayNode) {
+            console.warn("Unable to find displaynode")
+            return
+        }
+    
+        // Set image
+        const imageJson = JSON.parse(selectedImage)
+        if (imageAvailable) {
+            displayNode.setAttribute("src", `/img/${imageJson.FileName}`)
+        }
+    })
+    socket.onclose = (event) => {
+        body.style.background = "#501616"
 
-    // Message back
-    socket.send()
-})
+        // wait a little before each reboot
+        setTimeout(SetupSocketConnection, 10_000, true)
+    }
+}
+SetupSocketConnection(false)
