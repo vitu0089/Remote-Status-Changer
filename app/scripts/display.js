4425ee73b7757524d82b2518a3952bc94bdab740
vitu0089 <gamerw30@gmail.com>
2026-04-16 16:55:28 +0200

    Reconnect on display

--- app/scripts/display.js
+++ app/scripts/display.js
@@ -1,23 +1,33 @@
-// Websocket
-const socket = new WebSocket(`ws://${document.location.hostname}:61236`)
-socket.addEventListener("open", () => {
-    console.log("Socket connected successfully")
-})
+function SetupSocketConnection() {
+    // Websocket
+    const socket = new WebSocket(`ws://${document.location.hostname}:61236`)
 
-socket.addEventListener("message", (message) => {
-    let selectedImage = message.data
-    let imageAvailable = selectedImage != "None"
-    let displayNode = document.getElementById("displayImage")
-    if (!displayNode) {
-        // Create Image
-        displayNode = document.createElement("img")
-        displayNode.setAttribute("id", "displayImage")
-        document.body.appendChild(displayNode)
-    }
+    socket.addEventListener("open", () => {
+        console.log("Socket connected successfully")
+    })
+    
+    socket.addEventListener("message", (message) => {
+        let selectedImage = message.data
+        let imageAvailable = selectedImage != "None"
+        let displayNode = document.getElementById("displayImage")
+        if (!displayNode) {
+            // Create Image
+            displayNode = document.createElement("img")
+            displayNode.setAttribute("id", "displayImage")
+            document.body.appendChild(displayNode)
+        }
+    
+        // Set image
+        const imageJson = JSON.parse(selectedImage)
+        if (imageAvailable) {
+            displayNode.setAttribute("src", `/img/${imageJson.FileName}`)
+        }
+    })
 
-    // Set image
-    const imageJson = JSON.parse(selectedImage)
-    if (imageAvailable) {
-        displayNode.setAttribute("src", `/img/${imageJson.FileName}`)
+    socket.onclose = (event) => {
+        
+        // wait a little before each reboot
+        setTimeout(SetupSocketConnection, 10_000)
     }
-})
+}
+SetupSocketConnection()
